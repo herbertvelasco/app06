2d6de45e57ed1e95fcd8f3846ee7a6cddce8ecb0
HerbertVelasco <havm14@gmail.com>
2022-12-02 21:05:45 -0500

    2do avance

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,30 @@
+import 'dart:convert';
+import 'package:app06/ui/widges/item_pokemon_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-class HomePages extends StatelessWidget {
+class HomePages extends StatefulWidget {
+  @override
+  State<HomePages> createState() => _HomePagesState();
+}
+
+class _HomePagesState extends State<HomePages> {
+  List pokemons = [];
+  @override
+  void initState() {
+    super.initState();
+    getDataPokemon();
+  }
+
   getDataPokemon() async {
     Uri _uri = Uri.parse(
         "https://raw.githubusercontent.com/Biuni/PokemonGo-Pokedex/master/pokedex.json");
     http.Response response = await http.get(_uri);
+    if (response.statusCode == 200) {
+      Map<String, dynamic> myMap = json.decode(response.body);
+      pokemons = myMap["pokemon"];
+      setState(() {});
+    }
   }
 
   @override
@@ -32,19 +51,20 @@ class HomePages extends StatelessWidget {
                   height: 30.0,
                 ),
                 GridView.count(
+                  physics: const ScrollPhysics(),
                   shrinkWrap: true,
-                  crossAxisSpacing: 12,
-                  mainAxisSpacing: 12,
+                  crossAxisSpacing: 12.0,
+                  mainAxisSpacing: 12.0,
                   crossAxisCount: 2,
                   childAspectRatio: 1.35,
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Color(0xff4CCFB2),
-                        borderRadius: BorderRadius.circular(18.0),
-                      ),
-                    ),
-                  ],
+                  children: pokemons
+                      .map(
+                        (e) => itempokemonwidget(
+                          name: e["name"],
+                          image: e["img"],
+                        ),
+                      )
+                      .toList(),
                 ),
               ],
             ),
@@ -54,3 +74,4 @@ class HomePages extends StatelessWidget {
     );
   }
 }
+//http://www.serebeii.net/pokemongo/pokemon/001.png
